7e928bfd35e30f442b22e4e0075d028a78bddd9a
Bran18 <andreyfdez18@gmail.com>
2025-01-04 22:11:54 -0600

    chore: google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Providers } from "@/components/layout/providers";
 import "./globals.css";
 import RootLayoutWrapper from "@/components/layout/layout-helpers/root-layout-wrapper";
+import Head from "next/head";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -9,7 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "KindFi",
-  description: "The first Web3 platform connecting supporters to impactful causes while driving blockchain adoption for social and environmental change",
+  description:
+    "The first Web3 platform connecting supporters to impactful causes while driving blockchain adoption for social and environmental change",
 };
 
 export default function RootLayout({
@@ -19,6 +21,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-52DWMZ7R1H"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-52DWMZ7R1H');
+          `}
+        </script>
+      </Head>
       <body>
         <Providers>
           <RootLayoutWrapper>{children}</RootLayoutWrapper>
--- components/layout/layout-helpers/root-layout-wrapper.tsx
+++ components/layout/layout-helpers/root-layout-wrapper.tsx
@@ -2,7 +2,6 @@ import { AuthProvider } from '@/lib/hooks/use-auth'
 import { ThemeProvider } from 'next-themes'
 import { Header } from '../header/header'
 import Footer from '../footer/footer'
-import Head from 'next/head'
 
 export default function RootLayout({
   children,
@@ -10,35 +9,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
-      <Head>
-        {/* Google tag (gtag.js) */}
-        {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-52DWMZ7R1H"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-52DWMZ7R1H');
-          `}
-        </script>
-      </Head>
-      <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning>
-        <AuthProvider>
-          <ThemeProvider>
-            <div className="relative min-h-screen flex flex-col">
-              <Header />
-              <main className="flex-1">
-                {children}
-              </main>
-              <Footer />
-            </div>
-          </ThemeProvider>
-        </AuthProvider>
-      </body>
-      </html>
-    </>
+    <html lang="en" suppressHydrationWarning>
+    <body suppressHydrationWarning>
+      <AuthProvider>
+        <ThemeProvider>
+          <div className="relative min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">
+              {children}
+            </main>
+            <Footer/>
+          </div>
+        </ThemeProvider>
+      </AuthProvider>
+    </body>
+  </html>
   )
 }
