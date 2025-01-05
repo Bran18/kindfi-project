505a35df0d017345f9386070ffa361dd2576c18e
Bran18 <andreyfdez18@gmail.com>
2025-01-04 22:03:39 -0600

    chore: google tag

--- components/layout/layout-helpers/root-layout-wrapper.tsx
+++ components/layout/layout-helpers/root-layout-wrapper.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from '@/lib/hooks/use-auth'
 import { ThemeProvider } from 'next-themes'
 import { Header } from '../header/header'
 import Footer from '../footer/footer'
+import Head from 'next/head'
 
 export default function RootLayout({
   children,
@@ -9,20 +10,35 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-    <body suppressHydrationWarning>
-      <AuthProvider>
-        <ThemeProvider>
-          <div className="relative min-h-screen flex flex-col">
-            <Header />
-            <main className="flex-1">
-              {children}
-            </main>
-            <Footer/>
-          </div>
-        </ThemeProvider>
-      </AuthProvider>
-    </body>
-  </html>
+    <>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-52DWMZ7R1H"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-52DWMZ7R1H');
+          `}
+        </script>
+      </Head>
+      <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <AuthProvider>
+          <ThemeProvider>
+            <div className="relative min-h-screen flex flex-col">
+              <Header />
+              <main className="flex-1">
+                {children}
+              </main>
+              <Footer />
+            </div>
+          </ThemeProvider>
+        </AuthProvider>
+      </body>
+      </html>
+    </>
   )
 }
